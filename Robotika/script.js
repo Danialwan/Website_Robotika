8caf081b04969d52873e88070e0049149c94f729
Danialwan <79399739+Danialwan@users.noreply.github.com>
2021-04-21 03:53:38 -0700

    Delete Robotika directory

--- Robotika/script.js
+++
@@ -1,8 +0,0 @@
-let diagram = new dhx.Diagram("diagram_container", {
-    type: "org",
-    defaultShapeType: "img-card",
-    scale: 0.9
-  });
-  
-  if(screen.width <= 500) diagram.data.load('datasmall.json');
-  else diagram.data.load('databig.json');
